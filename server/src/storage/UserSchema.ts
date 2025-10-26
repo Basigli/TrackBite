26a634d8c7e933368278bdc24614d02c969fb95f
MHS <muhamad.salman2001@gmail.com>
2025-10-26 15:43:37 +0100

    feat: added saved recipes

--- server/src/storage/UserSchema.ts
+++ server/src/storage/UserSchema.ts
@@ -4,6 +4,7 @@ import { User } from '../models/User';
 const UserSchema: Schema<User> = new Schema({
   nickname: { type: String, required: true },
   mail: { type: String, required: true, unique: true },
+  savedRecipesIds: { type: [String], default: [] },
 });
 
 export const UserModel = mongoose.model<User>('User', UserSchema);

4f996a9fa1b9baf6ffb4dd6642c42ab73253e094
MHS <muhamad.salman2001@gmail.com>
2025-11-20 19:16:13 +0100

    chore: added mongo db credentials to docker run

--- server/mongo-setup.sh
+++ server/mongo-setup.sh
@@ -1,4 +1,4 @@
-docker run -d -p 27017:27017 mongo:latest
+docker run -d -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=pass mongo:latest
 
 # to inspect the db: 
 # docker exec -it test-mongo mongosh
@@ -6,4 +6,4 @@ docker run -d -p 27017:27017 mongo:latest
 ### mongosh:  
 # use food_test
 # show collections
-# db.dailyintakes.find().pretty()
+# db.dailyintakes.find().pretty()1ebb003b3f8c

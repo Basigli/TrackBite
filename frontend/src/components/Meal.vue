0f7302411c2f93afc1573c647c53be77a99416e3
MHS <muhamad.salman2001@gmail.com>
2025-11-22 22:26:00 +0100

    feat: added style to meals

--- frontend/src/components/Meal.vue
+++ frontend/src/components/Meal.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="meal border p-2 rounded mb-2">
-    <h3 class="font-semibold">{{ meal.name }}</h3>
-    <div class="foods ml-2">
+  <div class="meal bg-white rounded-xl shadow-md p-4 mb-4">
+    <h3 class="font-bold text-gray-800 text-lg mb-3">{{ meal.name }}</h3>
+    <div class="foods space-y-2">
       <FoodItem v-for="food in meal.foods" :key="food.id" :food="food"/>
     </div>
   </div>

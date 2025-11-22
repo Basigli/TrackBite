5b0d52bdc0a0adbf5eb074fc822719942f4352bd
MHS <muhamad.salman2001@gmail.com>
2025-11-22 22:24:51 +0100

    feat: added style to food items

--- frontend/src/components/FoodSearch.vue
+++ frontend/src/components/FoodSearch.vue
@@ -1,10 +1,25 @@
 <template>
-  <div class="food-search mb-4">
-    <input type="text" v-model="query" @input="searchFood" placeholder="Search for food..." class="border rounded px-2 py-1 w-full" />
+  <div class="food-search mb-4 max-w-md mx-auto">
+    <input
+      type="text"
+      v-model="query"
+      @input="searchFood"
+      placeholder="Search for food..."
+      class="w-full px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-300 shadow-sm"
+    />
 
-    <ul v-if="results.length" class="border rounded mt-2 max-h-48 overflow-y-auto">
-      <li v-for="food in results" :key="food.id" @click="selectFood(food)" class="p-2 hover:bg-gray-100 cursor-pointer">
-        {{ food.name }} - {{ food.calories }} kcal
+    <ul
+      v-if="results.length"
+      class="border border-gray-200 rounded-lg mt-2 max-h-48 overflow-y-auto shadow-lg bg-white"
+    >
+      <li
+        v-for="food in results"
+        :key="food.id"
+        @click="selectFood(food)"
+        class="px-4 py-2 hover:bg-green-100 cursor-pointer transition-colors flex justify-between"
+      >
+        <span>{{ food.name }}</span>
+        <span class="font-semibold text-gray-700">{{ food.calories }} kcal</span>
       </li>
     </ul>
   </div>

0225ba61d7b32173aa1b3663be01882ff4454df8
Gabriele Basigli <gabrielebasigli@gmail.com>
2025-11-25 17:06:01 +0100

    feat: added CORS support

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,10 @@
 import express from "express";
 const routes = require("./routes/routes");
 // import routes from "./routes/routes";
-
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use("/", routes);
 
 export { app };

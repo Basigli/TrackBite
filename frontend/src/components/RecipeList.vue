e91481b6802b5f4e9fba33f2d73d0525fe413473
MHS <muhamad.salman2001@gmail.com>
2025-12-08 18:38:39 +0100

    feat: allowing to add recipes to daily intakes

--- frontend/src/components/RecipeList.vue
+++ frontend/src/components/RecipeList.vue
@@ -12,28 +12,57 @@
       >
         <Recipe :recipe="recipe" />
 
-        <button
-          @click="deleteRecipe(recipe._id)"
-          class="text-red-500 hover:text-red-700 font-medium text-sm mt-1"
-        >
-          Delete
-        </button>
+        <div class="flex gap-2 mt-1">
+          <button
+            @click="deleteRecipe(recipe._id)"
+            class="bg-red-500 hover:bg-red-600 text-white font-medium text-sm px-3 py-1 rounded transition"
+          >
+            Delete
+          </button>
+
+          <button
+            @click="addRecipeToDailyIntake(recipe)"
+            class="bg-blue-500 hover:bg-blue-600 text-white font-medium text-sm px-3 py-1 rounded"
+          >
+            Add to Daily Intake
+          </button>
+        </div>
       </li>
     </ul>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { computed } from 'vue';
 import { useRecipeStore } from '../store/recipeStore';
+import { useIntakeStore } from '../store/intakeStore';
+import { useUserStore } from '../store/userStore';
 import Recipe from './Recipe.vue';
 
 const store = useRecipeStore();
+const intakeStore = useIntakeStore();
+const userStore = useUserStore();
+
 const recipes = computed(() => store.recipes);
 
 const emit = defineEmits(['delete-recipe']);
 
-const deleteRecipe = (recipeId) => {
+const deleteRecipe = (recipeId: string) => {
   emit('delete-recipe', recipeId);
+};
+
+const addRecipeToDailyIntake = async (recipe: any) => {
+  if (!userStore.user?._id) {
+    alert('Please log in to add to daily intake');
+    return;
+  }
+
+  const date = intakeStore.selectedDate;
+
+  for (const ingredient of recipe.ingredients) {
+    await intakeStore.addToDailyIntake(userStore.user._id, ingredient, date);
+  }
+
+  alert(`Added "${recipe.name}" and its ingredients to daily intake!`);
 };
 </script>

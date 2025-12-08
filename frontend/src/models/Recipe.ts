dcba8148698a524b617e11d27b36ee05ab698523
MHS-20 <muhamad.salman2001@gmail.com>
2025-12-08 14:41:10 +0100

    refactor: added new fields to recipe object

--- frontend/src/models/Recipe.ts
+++ frontend/src/models/Recipe.ts
@@ -1,16 +1,27 @@
 import { FoodItem, FoodItemSchemaZ } from "./FoodItem";
 import { z } from "zod";
+import { Nutrient, NutrientSchemaZ } from "./Nutrient";
 
 export interface Recipe {
   _id: string, 
   name: string,
   ingredients: Array<FoodItem>,
-  userId: string
+  description: string,
+  ownerId: string,
+  // image?: Buffer,
+  createdAt: Date,
+  grade: string,
+  macros: Array<Nutrient>
 }
 
 export const RecipeSchemaZ = z.object({
   _id: z.string().optional(),
   name: z.string(),
   ingredients: z.array(FoodItemSchemaZ),
-  userId: z.string()
+  ownerId: z.string(),
+  description: z.string(),
+  // image: z.any().optional(),
+  createdAt: z.date(),
+  grade: z.string(),
+  macros: z.array(NutrientSchemaZ)
 });

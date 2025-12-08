d4351a8c9a879894a94e999ac06606e6df72c7b9
Gabriele Basigli <gabrielebasigli@gmail.com>
2025-12-08 12:12:24 +0100

    refactor: added macros field to Diet model and schema

--- backend/src/models/Diet.ts
+++ backend/src/models/Diet.ts
@@ -1,15 +1,18 @@
 import { z } from "zod";
+import { Nutrient, NutrientSchemaZ } from "./Nutrient";
 
 export interface Diet {
   _id: string, 
   name: string,
   caloriesAmount: number,
+  macros: Array<Nutrient>,
   userId: string
 }
-
+  
 export const DietSchemaZ = z.object({
   _id: z.string().optional(),
   name: z.string(),
   caloriesAmount: z.number(),
+  macros: z.array(NutrientSchemaZ),
   userId: z.string()
 }).partial();
